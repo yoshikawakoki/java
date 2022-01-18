bc9909645701f5d4e573f8660f617e8036b74245
Koki Yoshikawa <cooler970708@gmail.com>
2022-01-18 13:44:08 +0900

    カリキュラム４章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String  text;
+		int num;
+		text = "吉川幸希";
+		System.out.println(text);
+		text = "動かざること山の如し";
+		System.out.println(text);
+		num = 1;
+		System.out.println(num);
+	}
+
+}
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,18 @@
+
+public class Variable01 {
+	public static void main(String[] args) {
+		int num1;
+		String strl;
+		num1 = 10;
+		strl = "Hello World";
+		System.out.println(num1);
+		System.out.println(strl);
+		
+		num1 = 20;
+		System.out.println(num1);
+		
+		num1 = 20;
+		System.out.println(num1);
+	}
+
+}
