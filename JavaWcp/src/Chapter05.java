3bf9b7d14895c038d361de59ad2017e566b98a47
Koki Yoshikawa <cooler970708@gmail.com>
2022-01-18 14:20:24 +0900

    カリキュラム５章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 2;
+		double num2 = 4.4;
+		boolean num3 = false;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		Float num4 = 5.4344f;
+		Integer num5 = 90;
+		Character num6 = 'あ';
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		int[] array1 = new int[] {40, 50, 60, 70, 80};
+		System.out.println(array1[3]);
+	}
+
+}
